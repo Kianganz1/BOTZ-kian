d216796e2843dbb362eddcf0dca599d4ac4e94cc
Kianganz1 <110093237+Kianganz1@users.noreply.github.com>
2022-07-27 19:54:59 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -7,10 +7,8 @@ let gopay = global.gopay
 let numberowner = global.numberowner
 let anu = `Hallo mypren 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-┌〔 Donasi • Emoney 〕
-├ Dana : ${dana}
+┌〔 Donasi • Pulsa 〕
 ├ Pulsa : ${pulsa}
-├ Gopay : ${gopay}
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 
@@ -19,7 +17,7 @@ Terimakasih yang sudah mendonasikan untuk bot
 Contact person Owner:
 wa.me/${numberowner} (Owner)
 
-*donasi via follow ig juga boleh*`
+*donasi via follow github juga boleh*`
   conn.sendButtonImg(m.chat, fla + teks, anu, instagram, 'Back', '.help', m) 
 }
 handler.help = ['donasi', 'donate']
